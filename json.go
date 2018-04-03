5c2476dde40f9576f1e6ba804738fe38ce8b50ea
idk <problemsolver@openmailbox.org>
2018-04-03 02:58:22 -0400

    experimenting with format

--- json.go
+++ json.go
@@ -22,11 +22,11 @@ func (j *jsonStructure) Format(m string, s ...string) string {
     rstring += "  params:"
     var params string
     for index, value := range s {
-        if index != 0 && index%2 == 0 {
+        if index != 0 && (index + 1)%2 == 0 {
             params += strings.TrimRight("  " + s[index-1] + ": \"" + value + "\"", " ") + "\n"
         }
     }
-    rstring += params + "\n"
+    rstring += params
     rstring += "}\n"
     return rstring
 }
